bbb442ac470d5b748a10102e7e68005bf80e262e
knekotic <alvarojosepl@gmail.com>
2015-06-23 09:16:11 +0200

    first commit, added element to error context menu

---
+++ soft.Hati.ErrorListSearchOn/soft.Hati.ErrorListSearchOn/SearchOn.cs
@@ -0,0 +1,113 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="SearchOn.cs" company="Hatisoft">
+//     Copyright (c) Hatisoft.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace soft.Hati.ErrorListSearchOn
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class SearchOn
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid MenuGroup = new Guid("36a6f571-5762-4e3b-baaa-37798d07777d");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly Package package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SearchOn"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        private SearchOn(Package package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException("package");
+            }
+
+            this.package = package;
+
+            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                CommandID menuCommandID = new CommandID(MenuGroup, CommandId);
+                EventHandler eventHandler = this.ShowMessageBox;
+                MenuCommand menuItem = new MenuCommand(eventHandler, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static SearchOn Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static void Initialize(Package package)
+        {
+            Instance = new SearchOn(package);
+        }
+
+        /// <summary>
+        /// Shows a message box when the menu item is clicked.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void ShowMessageBox(object sender, EventArgs e)
+        {
+            // Show a Message Box to prove we were here
+            IVsUIShell uiShell = (IVsUIShell)Package.GetGlobalService(typeof(SVsUIShell));
+            Guid clsid = Guid.Empty;
+            int result;
+            Microsoft.VisualStudio.ErrorHandler.ThrowOnFailure(uiShell.ShowMessageBox(
+                0,
+                ref clsid,
+                "SearchOnPackage",
+                string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName),
+                string.Empty,
+                0,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST,
+                OLEMSGICON.OLEMSGICON_INFO,
+                0,        // false
+                out result));
+        }
+    }
+}
---
+++ soft.Hati.ErrorListSearchOn/soft.Hati.ErrorListSearchOn/SearchOnPackageGuids.cs
@@ -0,0 +1,21 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="SearchOnPackageGuids.cs" company="Hatisoft">
+//         Copyright (c) Hatisoft.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace soft.Hati.ErrorListSearchOn
+{
+    using System;
+
+    /// <summary>
+    /// SearchOn GUID constants.
+    /// </summary>
+    internal static class SearchOnPackageGuids
+    {
+        /// <summary>
+        /// SearchOn GUID string.
+        /// </summary>
+        public const string PackageGuidString = "fa7c5b9c-1d8b-4ff3-8d39-09cc50c4ab7c";
+    }
+}
